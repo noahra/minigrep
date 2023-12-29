8ad4591b59370094841f8f0b3f38f6b364ee317f
nora <nora@netlight.com>
2023-12-29 10:19:54 +0100

    Search functionality implemented.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use minigrep::Config;
 use std::env;
 use std::process;
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
